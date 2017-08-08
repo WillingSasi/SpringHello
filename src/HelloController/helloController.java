82494472f169819169413c9203100700c48df6ab
guanhua.liu <guanhua.liu>
2017-08-08 15:53:42 +0800

    Initial commit

---
+++ src/HelloController/helloController.java
@@ -0,0 +1,18 @@
+package HelloController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/hello",method = RequestMethod.GET)
+public class helloController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String printHello(ModelMap modelMap){
+        modelMap.addAttribute("msg","Spring MVC Hello World!");
+        modelMap.addAttribute("name","liuguanhua");
+        return "hello";
+    }
+}
